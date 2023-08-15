e9cef3f11d07e7a80b40ee4606044f86340abb1f
David C Ellis <ducksual@gmail.com>
2023-08-15 09:56:56 +0100

    Test the spec is being read as expected.

---
+++ tests/test_spec.py
@@ -0,0 +1,26 @@
+import pathlib
+
+import pytest
+
+from snakerun.core import DependencySpec
+
+examples_folder = pathlib.Path("../examples")
+
+specs = [
+    ("pep_example_script.py", DependencySpec(pyver=None, dependencies=['requests', 'rich'])),
+    ("prefab_demo.py", DependencySpec(pyver=">=3.10", dependencies=['prefab_classes'])),
+    ("requires_py39.py", DependencySpec(pyver="~=3.9.0", dependencies=[])),
+    ("requires_py310.py", DependencySpec(pyver="~=3.10.0", dependencies=[])),
+    ("requires_py311.py", DependencySpec(pyver="~=3.11.0", dependencies=[])),
+    ("requires_gt_39.py", DependencySpec(pyver=">=3.9", dependencies=[])),
+    ("requires_nothing.py", DependencySpec(pyver=None, dependencies=[])),
+]
+
+
+@pytest.mark.parametrize("path, spec", specs)
+def test_spec_matches(path, spec):
+    script_file = examples_folder / path
+
+    deps = DependencySpec.from_script(script_file)
+
+    assert deps == spec
