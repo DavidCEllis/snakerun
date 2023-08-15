19b10f8dbc07440bf6e4806accb9eb599c08b715
David C Ellis <ducksual@gmail.com>
2023-08-15 10:55:37 +0100

    Shorten docstring

--- src/snakerun/__init__.py
+++ src/snakerun/__init__.py
@@ -2,24 +2,7 @@
 SnakeRun
 
 Parse a meaningful comment form to get the requirements for a python script.
-Check the Python Version matches a minimum requirement.
-Check for cached environments that match and use those if available.
-Otherwise make a new venv and install the requirements.
-Launch the script in that environment.
-
-The cache is decided by the text of the requirements in the script and *not*
-the contents of the environment or the script itself. This is so multiple
-scripts with exactly the same dependencies can reuse the same venv.
-
-If a version of a dependency is specified then only environments created
-with that exact specification will match (eg: 'requests>2.28' won't reuse
-an environment created with 'requests' even if the actual version installed
-satisfies the requirement). This is purely to make the comparison as simple
-and fast as possible in the 'good' case where the dependencies match.
-
-If pyenv is installed and a pyenv environment is available that satisfies
-the python version specification, that version will be used to generate
-the virtualenv.
+Launch a python environment matching those requirements.
 
 Usage:
 `snakerun your_script.py`
